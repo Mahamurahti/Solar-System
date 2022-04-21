36b42d88d6c65e524e307f9407e8818ce5d92555
Eric <55787260+Mahamurahti@users.noreply.github.com>
2022-04-21 11:44:59 +0300

    Font will be loaded only once
    Improves performance

--- components/SolarSystem.js
+++ components/SolarSystem.js
@@ -212,25 +212,30 @@ export default function SolarSystem() {
             const intersects = raycaster.intersectObjects(objects, false)
             if (intersects.length > 0) {
                 cameraTarget = intersects[0].object
-                const fontLoader = new FontLoader();
-                fontLoader.load(robotoFontPath, function (font) {
-                    scene.remove(text)
-                    const fontMaterial = new THREE.LineBasicMaterial({
-                        color: 0xff0000,
-                    });
-                    const message = getDescription(cameraTarget.name)
-                    const shapes = font.generateShapes(message, 4);
-                    const geometry = new THREE.ShapeGeometry(shapes);
-                    geometry.computeBoundingBox();
-                    const xMid = -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
-                    geometry.translate(xMid, 0, 0);
-                    text = new THREE.Mesh(geometry, fontMaterial)
-                    description = text
-                    scene.add(text)
-                })
+                scene.remove(text)
+                description = generateDescription()
+                scene.add(text)
             } else {
                 if (!moved) scene.remove(text)
             }
+        }
+
+        const fontLoader = new FontLoader()
+        let font
+        fontLoader.load(robotoFontPath, function (robotoFont) {
+            font = robotoFont
+        })
+
+        function generateDescription() {
+            const fontMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+            const message = getDescription(cameraTarget.name)
+            const shapes = font.generateShapes(message, 4);
+            const geometry = new THREE.ShapeGeometry(shapes);
+            geometry.computeBoundingBox();
+            const xMid = -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
+            geometry.translate(xMid, 0, 0);
+            text = new THREE.Mesh(geometry, fontMaterial)
+            return text
         }
 
         let description = null;
