9ae56a8d198734df2572667042bd21a37e6f2594
Eric <55787260+Mahamurahti@users.noreply.github.com>
2022-04-25 13:20:21 +0300

    Update createDescription.js

--- components/SolarSystem/createDescription.js
+++ components/SolarSystem/createDescription.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import getDescription from "../../helpers/getDescription";
+import * as THREE from "three"
+import getDescription from "../../helpers/getDescription"
 
 /**
  * Creates description for celestial body.
@@ -10,13 +10,15 @@ import getDescription from "../../helpers/getDescription";
  * @returns {Mesh}
  */
 export default function createDescription(font, target) {
-    const fontMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    const fontMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 })
     const message = getDescription(target.name)
-    const shapes = font.generateShapes(message, 4);
-    const geometry = new THREE.ShapeGeometry(shapes);
-    geometry.computeBoundingBox();
-    const xMid = -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
-    geometry.translate(xMid, 0, 0);
+    const shapes = font.generateShapes(message, 4)
+    
+    const geometry = new THREE.ShapeGeometry(shapes)
+    geometry.computeBoundingBox()
+    const xMid = -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x)
+    geometry.translate(xMid, 0, 0)
+
     const description = new THREE.Mesh(geometry, fontMaterial)
     return description
 }
